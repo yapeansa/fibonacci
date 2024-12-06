35886e848f87b65f75ab4d33804030eecd40b991
yapeansa <yago.pereira@estudante.ufjf.br>
2024-12-05 22:33:37 -0300

    Calculando o tempo de execução para duas versões de Fibonacci: iterativo e recursivo.

--- fibonacci_recursivo/main.c
+++ fibonacci_recursivo/main.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
+#include <time.h>
 #include "fibonacci.h"
 
 int main()
 {
     int n;
+    clock_t start, end;
 
     printf("Quantos termos da sequência de Fibonacci você deseja exibir? ");
     scanf("%d", &n);
 
+    start = clock();
     lista_fibonacci(n);
+    end = clock();
+
+    double duracao = ((double)end - start) / CLOCKS_PER_SEC;
+    printf("\nO tempo gasto para Fibonacci recursivo foi: %f segundos.\n", duracao);
 
     printf("\n");
     return 0;
